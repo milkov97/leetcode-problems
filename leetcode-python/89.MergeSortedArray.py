6ec7a7e1f8f4b4fabac3d3c3b8e4c47329770fbe
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-25 18:06:40 +0300

    add it to a class method

--- leetcode-python/89.MergeSortedArray.py
+++ leetcode-python/89.MergeSortedArray.py
@@ -1,10 +1,8 @@
-def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for j in range(n):
-            nums1[m+j] = nums2[j]
+        for i in range(n):
+            nums1[m+i] = nums2[i]
         nums1.sort()
-        return nums1
-
-print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
